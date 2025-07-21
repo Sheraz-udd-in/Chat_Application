2b0fa2e102458c7b163f528bfaf5ee1a0a13a685
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-21 14:46:24 +0530

    Websocket URL

--- chatapp/app/urls.py
+++ chatapp/app/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('logout/', logoutView, name='logout'),
     path('register/', register, name='register'),
 ]
+from django.urls import re_path
+from . import consumer
+
+websocket_urlpatterns = [
+   re_path(r'ws/chat/(?P<room_id>\w+)/$', consumer.ChatConsumer.as_asgi()),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
